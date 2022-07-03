ccb073cb5199d6c793af9ae425ce2e4821910fa2
cfa532 <cfa532@yahoo.com>
2022-07-04 00:25:30 +0800

    api handler

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -1,69 +1,107 @@
-<script setup lang="ts">
+<script lang="ts">
 // import Uploader from "./Uploader.vue"
 // import FileList from "./FileList.vue"
 
+import { stringifyStyle } from "@vue/shared";
+import { defineComponent, provide } from "vue";
 console.log("manager.js")  
-var global
-var c = localStorage.getItem("selected")
-if (c == null){
-    c = "system"
-}
 
-export default {
+declare global {
+    interface Window {          //must be cap W
+        lapi: any;
+        getParam: any;
+        hprose: any
+    }
+};
+
+export default defineComponent({
     name: "Manager",
-    // el: '#leitherbody',
     components: {
-        headnodestate, Uploader, FileList
-        },
+    //     headnodestate, Uploader, FileList
+    },
     data() {
         return {
-            ips:"",
             isLogined:true,
-            selected: c,
-            sid:"",
-            mynodedata:{
-                nodeid:" ",
-                hostpk:" ",
-                ips:" ",
-                gateway:"  ",
-                peerid:" ",
-                swarmlocal:""
-            },
-            swarmdata:{
-                dhts:[],
-                //peers:[],
-                addrs:[],
-                gateway:"  "
-            },
-            appsdata:{
-                apps:[]
-            },
-            systemdata:{
-                localnodeid:"",
-                oldVars:"",
-                systemvars:"",
-                bChanged: false        
-            }
         }
     },
     methods: {
-        select: name=> {
-            global.vue.selected = name
-            localStorage.setItem("selected", name)
-        },
     },
     created() {
-        global = window     // which is used inside auth.js as global
-        global.vue = this.$data
-        import("/src/auth.js").then((module) => {
-            console.log("module", module);
-        })
+        const ayApi = ["GetVarByContext", "Act", "Login", "Getvar","Getnodeip", "SwarmLocal","DhtGetAllKeys",
+            "DhtGet", "DhtGets", "SignPPT", "SwarmAddrs", "MFOpenTempFile", "MFTemp2MacFile", "MFSetData",
+            "MFGetData", "MMCreate", "MMOpen", "Hset", "Hget", "Zadd", "Zrangebyscore", "Zrange", "MFOpenMacFile"]
+
+        function getcurips(){
+            //缺省的地址，用于本地调试程序
+            let ips = "127.0.0.1:4800"
+            //获取节点链接
+            if (window.getParam != null){
+                let p=window.getParam()
+                console.log("p=", p)
+                ips = p["ips"][p.CurNode]
+                // hosturl = "ws://" + p["ips"][p.CurNode] + "/ws/"
+                // baseurl = "http://" + p["ips"][p.CurNode] + "/"
+            } else if (window.location.host != ""){
+                ips = window.location.host
+                // hosturl = "ws://" + window.location.host + "/ws/"
+                // baseurl = "http://" +  window.location.host + "/"
+            }
+            { //for test
+                ips = "192.168.1.104:4800"
+            }
+            // window.lapi.ips = ips
+            return ips
+        }
+
+        let getLocalApiHandler = ()=>{
+            let apiHandler: any = {};
+            let ips = getcurips()
+            apiHandler.ips = ips
+            let hosturl = "ws://" + ips +"/ws/"
+            let baseurl = "http://" + ips + "/"
+            //生成操作句柄
+            apiHandler.client = window.hprose.Client.create(hosturl, ayApi);
+
+            //以上部分可以提取公用代码
+            return apiHandler.client.GetVarByContext("", "ppt")
+            .then((ppt:string)=>{
+                showPPT(ppt)
+                return apiHandler.client.Login(ppt)
+                })
+            .then((reply:any)=>{
+                apiHandler.isLogined = true
+                apiHandler.baseUrl = baseurl
+                apiHandler.sid = reply.sid
+                apiHandler.leitherid = reply.uid
+
+                console.log("sid=", reply.sid)
+                console.log("uid=", reply.uid)
+                return apiHandler
+                //查询应用            
+                //showapps(sid)
+                })
+        }
+        window.lapi = getLocalApiHandler()
+
+        //读出通行证中的userid
+        function showPPT(ppt: string){
+            console.log("ppt=", ppt)
+            var p = JSON.parse(ppt)
+            // for (k in ppt){
+            //     console.log("k=", k)        
+            // }
+            console.log("data=", p.Data)
+
+        }
+        // import("/src/auth.js").then((module) => {
+        //     console.log("module", module);
+        // })
         // var tag = document.createElement("script");
         // tag.setAttribute("src", "/src/auth.js");
         // tag.setAttribute("type", 'text/javascript');
         // document.head.appendChild(tag); 
     },
-}
+})
 </script>
 <template>
 </template>
