bdc82a4b682c342578bc61faeec4850cdebef588
cfa532 <cfa532@yahoo.com>
2022-08-27 11:12:42 +0800

    slicesize -1

--- src/components/Gadget/Image.vue
+++ src/components/Gadget/Image.vue
@@ -5,7 +5,6 @@ const props = defineProps({
     macid : {type: String, required: false},
     fileType: {type: String, required: false}
 })
-const sliceSize = 1024 * 1024 * 10
 const imageUrl = ref("")
 const img = ref<HTMLImageElement>()
 onMounted(async () => {
@@ -15,9 +14,8 @@ onMounted(async () => {
 async function getLink():Promise<string> {
     return new Promise((resolve)=>{
         api.client.MFOpenMacFile(api.sid, api.mid, props.macid, (fsid: string) => {
-            const fileType = props.fileType
-            api.client.MFGetData(fsid, 0, sliceSize, (buf:  ArrayBuffer) => {
-                const blob = new Blob([buf], {type: fileType as string});
+            api.client.MFGetData(fsid, 0, -1, (buf:  ArrayBuffer) => {
+                const blob = new Blob([buf], {type: props.fileType});
                 resolve(URL.createObjectURL(blob))
             }, (err: Error) => {
                 console.error("Get File data error=", err)
--- src/components/Gadget/pdf.vue
+++ src/components/Gadget/pdf.vue
@@ -6,29 +6,18 @@ const props = defineProps({
     macid : {type: String, required: true},
     fileType: {type: String, required: true}
 })
-const sliceSize = 1024 * 1024 * 10
 onMounted(() => {
     api.client.MFOpenMacFile(api.sid, api.mid, props.macid, (fsid: string) => {
-        readData2Buf(fsid, 0, Array.from(new Uint8Array(0)))
+        api.client.MFGetData(fsid, 0, -1, (buf:  ArrayBuffer) => {
+            const blob = new Blob([buf], {type: props.fileType});
+            pdfobject.embed(URL.createObjectURL(blob), "#pdfviewer", { height: "60rem" })
+        }, (err: Error) => {
+            console.error("Get File data error=", err)
+        })
     }, (err: Error) => {
         console.error("Open file error=", err)
     })
 })
-function readData2Buf(fsid: string, start: number, d: any[]) {
-    const fileType = props.fileType
-    api.client.MFGetData(fsid, start, sliceSize, (buf:  ArrayBuffer) => {
-        d = d.concat(Array.from(new Uint8Array(buf)))
-        if (buf.byteLength < sliceSize) {
-            // end of data stream
-            const blob = new Blob([new Uint8Array(d)], {type: fileType});
-            pdfobject.embed(URL.createObjectURL(blob), "#pdfviewer", { height: "60rem" })
-        } else {
-            readData2Buf(fsid, start+sliceSize, d)
-        }
-    }, (err: Error) => {
-        console.error("Get File data error=", err)
-    })
-}
 </script>
 
 <template>
