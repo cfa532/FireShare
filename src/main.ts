1f7852481019db5120996db1332f79fd0b0e0791
cfa532 <cfa532@yahoo.com>
2022-09-14 18:43:07 +0800

    add router directory

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,15 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createRouter, createWebHashHistory } from 'vue-router';
-// import { getLApi } from './auth';
 import { useLeither } from './stores/lapi';
 import App from './App.vue';
-import FileListVue from './components/FileList.vue';
-import MainPageVue from './components/MainPage.vue';
-import FileViewVue from './components/FileView.vue';        // Mac files within a MM
-import FileView2Vue from './components/FileView2.vue';      // view files in webdav under Leither
-// const FileViewVue = ()=> import('./components/FileView.vue')       //lazy load, may cause error in Leither
+import { router } from './router'
 
-const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHashHistory(),
-    routes: [
-        { path: '/', name:"main", component: MainPageVue},
-        { path: '/filelist/:page?', name:"filelist", component: FileListVue},
-        { path: '/fileview/:macid/:fileType', name:"fileview", component: FileViewVue},
-        { path: '/fileview2/:filePath', name:"fileview2", component: FileView2Vue},
-    ],
-})
 const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
 app.config.unwrapInjectedRef=true       // temp setting until ver 3.3
 app.use(router)
-useLeither().login().then(
-    ()=>app.mount("#app"),
+useLeither().login().then(()=>app.mount("#app"),
     (err: Error)=>{
         console.error("Login err=", err)
     })
-
-
-// useLeither().getLApi().then((api:any)=>{
-//     // window.lapi = api
-//     // app.provide("lapi", api)    // global Leither apiHandler
-//     app.mount("#app")
-// }, (err:Error)=> {
-//     console.error("Error getLapi", err)
-// })
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import { useLeither } from "../stores/lapi";
+import FileListVue from '../components/FileList.vue';
+import MainPageVue from '../components/MainPage.vue';
+import FileViewVue from '../components/FileView.vue';        // Mac files within a MM
+import FileView2Vue from '../components/FileView2.vue';      // view files in webdav under Leither
+
+export const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    linkActiveClass: 'active',
+    routes: [
+        { path: '/', name:"main", component: MainPageVue},
+        { path: '/filelist/:page?', name:"filelist", component: FileListVue},
+        { path: '/fileview/:macid/:fileType', name:"fileview", component: FileViewVue},
+        { path: '/fileview2/:filePath', name:"fileview2", component: FileView2Vue},
+    ],
+});
+
+router.beforeEach(async (to) => {
+    const authLeither = useLeither();
+    // before each route change, check use authority
+})
