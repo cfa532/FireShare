e5d7968bf547789ac9b855caed951a922a1a3d52
cfa532 <cfa532@yahoo.com>
2022-09-18 21:40:35 +0800

    Update Share.vue

--- src/components/Gadget/Share.vue
+++ src/components/Gadget/Share.vue
@@ -7,27 +7,29 @@ const classModal = reactive<CSSProperties>({
   'z-index': 1,
   overflow: "auto",
   left: 0, top: 0, width: "100%", height: "100%",
-  'background-color': "rgb(0,0,0,0.4)",
+//   'background-color': "rgb(0,0,0,0.4)",
 });
+
+
+
 const shareMenu = ref()
 function showShareMenu() {
-    shareMenu.value.hidden = false;
+    classModal.display = "block"
 }
 window.onclick = function (e: MouseEvent) {
-        console.log(e.target, shareMenu.value)
-        if (e.target !== shareMenu.value) {
-            shareMenu.value.hidden = true;
-        }
+    if (e.target == shareMenu.value) {
+        classModal.display = "none"
     }
+}
 </script>
 
 <template>
 <div style=" width:100%; position: flex; text-align: right;">
     <a href="" @click.prevent="showShareMenu" style="font-size: 20px; color:lightgrey">&#8226; &#8226; &bull;</a>
-    <div id="myModal" :style="classModal">
-    <div ref="shareMenu" style="position:absolute; top:0%; right: 0%; background-color: aqua;
-        width: 300px; height: 400px">
-    </div>
+    <div ref="shareMenu" :style="classModal">
+        <div style="position:absolute; top:0%; right: 0%; background-color: aqua;
+            width: 300px; height: 400px">
+        </div>
     </div>
 </div>
 </template>
