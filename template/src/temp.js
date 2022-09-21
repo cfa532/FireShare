63fc540cc5f13aa2e3450261ad92067f7d1fd260
cfa532 <cfa532@yahoo.com>
2022-09-21 12:07:08 +0800

    temp file works
    with ipv6 support and fetch

--- template/src/temp.js
+++ template/src/temp.js
@@ -1,118 +1,82 @@
 function f(){
     var p 
-    function getParam() {              
+    window["getParam"] = ()=>{              
         return p
     }
-    window["getParam"] = getParam    
-    // function initParam(){
-    //     var bSameSubnet = false
-    
-    //     var p=window.getParam()
-    //     for (i=0; i< p.ips.length; i++){
-    //         var ip = p.ips[i].split(":")[0]
-    //         if (ip == p.remote){
-    //             bSameSubnet = true
-    //             break
-    //         }
-    //     }
-    //     for (i=0; i< p.ips.length; i++){
-    //         var ip = p.ips[i].split(":")[0]        
-    //         //如果是同一网络，返回局域网
-    //         if (bSameSubnet && !isPubNetAddr(ip)){
-    //             p.CurNode = i
-    //             break
-    //         }
-    //         //如果不是同一网络,返回互联网
-    //         if (!bSameSubnet && isPubNetAddr(ip)){
-    //             p.CurNode = i
-    //             break
-    //         }
-    //     }
-    // }
-    function initParam(){    
-        var p=window.getParam()
-        for (i=0; i< p.ips.length; i++){
-            var ip = p.ips[i].split(":")[0]        
-            //优先返回公网地址
-            if (isPubNetAddr(ip)){
-                p.CurNode = i
-                break
-            }
-        }
-    }
-
-    //window["initParam"] = initParam
-    window["setParam"] = function(param){
+    window["setParam"] = (param)=>{
         p = param
         //window.initParam()
         initParam()
     }
-    function isPubNetAddr(host){
-        var f = host.split(".")[0]
-        var ret =  f==10 || f==192 || f== 172
-        return !ret
+    function initParam(){    
+        // get public IPv4, or v6 if no v4
+        var p=window.getParam()
+        for (i=0; i< p.ips.length; i++){
+            var ip = p.ips[i].split(":")
+            if (ip.length==2) {
+                // IPv4 address, 1.172.121.64:4800
+                var a = ip[0].split('.')
+                if ( a[0]==10 || (a[0]==192 && a[1]==168) || (a[0]==172 && a[1>15 && a[1]<32])) {
+                    // private IPv4 for sure
+                    continue;
+                } else {
+                    // public IPv4
+                    p.CurNode = i;
+                    break;
+                }
+            } else {
+                // public IPv6, [2001:b011:e608:33c7:497d:480:498:45b8]:4800"
+                // private, fe80::3509:974:e443:8b8c
+                var a = p.ips[i].replace(/\[|\]/g,'').split(':');
+                if (a.length < 8) {
+                    // private IPv6
+                    continue;
+                } else {
+                    // public IPv6
+                    p.CurNode = i 
+                }
+            }
+        }
     }
-
     function getUrl(param, index) {
         url ="http://"+ param["ips"][index] +"/entry?"
         if (param["aid"] != ""){
             url += "aid="+ param["aid"] + "&"
         }
-    
         if (param["mid"] != ""){
             url += "mid="+ param["mid"] + "&"
         }
-    
         if (param["ver"] != ""){
             url += "ver="+ param["ver"] + "&"
         }
-        
         url +=   "rand=" + Math.random()   
-        return url
-
-        // return  "http://"+ param["ips"][index] +"/entry?"
-        //     + "appid="+ param["id"] + "&"    
-        //     //+ "author=" + param["author"] +"&"
-        //     //+ "app="+ param["app"] + "&"
-        //     + "ver="+param["ver"] +  "&"
-        //     + "mid="+param["mid"] +  "&"
-        //     + "rand=" + Math.random()       
+        return url 
     }
-    function requestUrl(url){
-	    CurNodeUrl = url	
-        var xhr = new XMLHttpRequest();
-        if (xhr.withCredentials === undefined) {
-            err = "XMLHttpRequest not support!"
-            console.log(err)
-            return err
-        }
-        xhr.onreadystatechange = function(){
-            if (xhr.readyState == 4){
-                if (xhr.status==200)  {// 200 = "OK"
-                    var html = document.getElementById("LeitherHtml");          
-                    var reg = /<html\s*\S*>([\s|\S]*)<\/html>/ig;
-                    xhr.responseText.replace(reg, function() {                 
-                        html.innerHTML = arguments[1]
-                    });             
-                    var regsrc = /<script[^>]*>([\s|\S]*?)<\/script>/igm;    
-                    while ((result = regsrc.exec(xhr.responseText))!= null){
-                        var script = document.createElement("script");
-                        script.textContent = result[1]
-                        document.getElementsByTagName("head")[0].appendChild(script);
-                    }        
-                }
-                else{
-    //                console.log("Problem retrieving XML data:" + xhr.statusText);
-                }               
-            }     
-        }
-        xhr.open("GET", url, true)    
-        xhr.send(null)
+    function fetchUrl(url) {
+        fetch(url, {CurNodeUrl:url}).then(resp=>{
+            if (!resp.ok) {
+                throw new Error('HTTP error status: ${resp.status}');
+            }
+            return resp.text()
+        }).then(txt=>{
+            var html = document.getElementById("LeitherHtml");          
+            var reg = /<html\s*\S*>([\s|\S]*)<\/html>/ig;
+            txt.replace(reg, function() {                 
+                    html.innerHTML = arguments[1]
+            });             
+            var regsrc = /<script[^>]*>([\s|\S]*?)<\/script>/igm;    
+            while ((result = regsrc.exec(txt))!= null){
+                var script = document.createElement("script");
+                script.textContent = result[1]
+                document.getElementsByTagName("head")[0].appendChild(script);
+            }
+        }).catch(r=>{
+            console.log(r)
+        })
     }
     function requestEntry() {        
         var url = getUrl(p, p.CurNode)
-        console.log(url)
-        return requestUrl(url)    
+        return fetchUrl(url)
     }
     window["requestEntry"] = requestEntry    
 }
