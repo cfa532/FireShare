23220da66f1703b4ee64bea7222b3e2baaf49b57
cfa532 <cfa532@yahoo.com>
2024-02-22 17:54:47 +0800

    better tlink

--- src/views/t.vue
+++ src/views/t.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch, shallowRef } from 'vue'
 import { useRoute } from 'vue-router';
 import { useLeither, useMimei } from '../stores/lapi';
 import { Html as Page, VideoJS, EditorModal, PDFView } from '../components/index'
@@ -11,43 +11,44 @@ const showEditor =  ref("none")
 const columnTitle = ref("t")    // special column for upload temp files
 const mid = route.params.id     // if none, show upload button. If valid id, display the file page.
 const fileView = ref<HTMLDivElement>()
-const userComponent = ref()
+const userComponent = shallowRef()
 const params = ref()
 
 onMounted(async () => {
-    if (mid) {
-        const fi = await api.client.Hget(await mmInfo.mmsid, columnTitle.value, mid)
-        console.log(mid, fi)
-        // display file content or download it.
-        if (fi.type.includes("image")) {
-            fileView.value!.hidden = false
-            userComponent.value = Image
-        } else if (fi.type.includes("pdf")) {
-            fileView.value!.hidden = false
-            userComponent.value = PDFView
-        } else if (fi.type.includes("video") || fi.type.includes("audio")) {
-            fileView.value!.hidden = false
-            userComponent.value = VideoJS
-        } else if (fi.type.includes("page")) {
-            // webpage that includes text and files
-            fileView.value!.hidden = false
-            userComponent.value = Page
-        } else {
-            console.warn("Unknown file type:", fi.type)
-            return "<p>Unkonwn file type</P>"
-        }
-        params.value = { title: columnTitle.value, mid: fi.mid, fileType: fi.type, fileName: fileName(fi)}
-    }
+    if (mid) await load(mid as string)
 })
 function fileName(file: FileInfo):string {
     return file.caption? file.caption : file.name;
 }
-
+async function load(mid:string) {
+    const fi = await api.client.Hget(await mmInfo.mmsid, columnTitle.value, mid)
+    console.log(mid, fi)
+    // display file content or download it.
+    if (fi.type.includes("image")) {
+        fileView.value!.hidden = false
+        userComponent.value = Image
+    } else if (fi.type.includes("pdf")) {
+        fileView.value!.hidden = false
+        userComponent.value = PDFView
+    } else if (fi.type.includes("video") || fi.type.includes("audio")) {
+        fileView.value!.hidden = false
+        userComponent.value = VideoJS
+    } else if (fi.type.includes("page")) {
+        // webpage that includes text and files
+        fileView.value!.hidden = false
+        userComponent.value = Page
+    } else {
+        console.warn("Unknown file type:", fi.type)
+        return "<p>Unkonwn file type</P>"
+    }
+    params.value = { title: columnTitle.value, mid: fi.mid, fileType: fi.type, fileName: fileName(fi)}
+}
 const tlink = ref()
 function uploaded(fi: FileInfo) {
     // a file is uploaded, display it link
-    console.log(fi)
-    tlink.value = window.location.href+"/"+fi.mid
+    console.log(window.location)
+    const url = window.location.href
+    tlink.value = url.substring(0, url.indexOf('/t'))+"/t/"+fi.mid
     showEditor.value = "none"
     api.logout()
 }
@@ -55,13 +56,16 @@ async function upload() {
     showEditor.value = "block"
     await api.login("gen8", "123456")
 }
+watch(()=>route.params.id, async (nv)=>{
+    if (nv) await load(nv as string)
+})
 </script>
 <template>
     <div ref="fileView" hidden>
         <component :is="userComponent" v-bind="params"></component>
     </div>
     <hr>
-    <button @click.prevent="upload">Upload</button>
+    <button class="btn btn-link" @click.prevent="upload">Upload</button>
     <br>
     <a :href="tlink">{{ tlink }}</a>
 
