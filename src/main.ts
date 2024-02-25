056cdf983ca5d62d2d067286b58ab0fa881d1159
cfa532 <cfa532@yahoo.com>
2024-02-25 16:13:31 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
+app.config.errorHandler = (err, instance, info) => {
+    // Handle the error globally
+    console.error("Global error:", err);
+    console.log("Vue instance:", instance);
+    console.log("Error info:", info);
+}
 
 console.warn("main.ts built....on " + __BUILD_TIME__, "ver:"+import.meta.env.VITE_APP_VERSION)
 app.mount("#app")
