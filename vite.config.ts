42173e017b5c4993e62067e5fa6e897b658cd653
cfa532 <cfa532@yahoo.com>
2024-03-07 23:04:22 +0800

    remove console

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from "vite-plugin-singlefile"
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [vue(), viteSingleFile({inlinePattern: ["*.css"]})],
+    plugins: [vue(), viteSingleFile({inlinePattern: ["*.css"]}), removeConsole({ includes: ["log"]})],
     build: {
       assetsDir: '.',   // create only one layer of directory structure
-      cssCodeSplit: true
+      cssCodeSplit: true,
     },
     resolve: {
       alias: {
@@ -19,6 +20,6 @@ export default defineConfig(({ command, mode }) => {
     define: {
       // declare it in env.d.ts, otherwise won't build
       __BUILD_TIME__: JSON.stringify(new Date().toISOString()),   // https://vitejs.dev/config/shared-options.html
-    }
+    },
   }
 })
