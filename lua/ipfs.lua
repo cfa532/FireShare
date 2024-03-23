880b7e6d5648b0373eab5eb863b45e3b671ba2d5
cfa532 <cfa532@yahoo.com>
2024-03-24 00:25:24 +0800

    Create ipfs.lua

---
+++ lua/ipfs.lua
@@ -0,0 +1,57 @@
+sid =request['sid']
+print("mimei sid", sid)
+local mm = require('mimei');
+
+mid = "O6mabg95qj6WlKU105gaHMQloFW"	-- db mimei
+mmsid, err = mm.MMOpen(sid, mid, 'cur');
+if (err ~= nil) then
+	print('MMOpen err',  err);
+	return err
+end
+print('MMOpen mmsid=', mmsid);
+
+local key = "Videos"
+local score = os.time()
+local field0 = "QmZcJMzg5WDFgCZmYzgmTXpEvxFQCvBUzLYxwN21TD1ENV"
+local fv0 = 
+{
+	lastModified= score,
+	mid= field0,
+	size= 2487754909,
+	type= "video/mp4",
+	name= "3.body.problem.S01E01.mkv",
+	caption= "3.body.problem.S01E01"
+}
+
+--hmset hmget
+local sp = scorepair.new(score, field0)
+ret, err = mm.ZAdd(mmsid, key, sp);
+if (err ~= nil) then
+    print("ZAdd error=", err)
+end
+
+local fvs, err = mm.HSet(mmsid, key, field0, fv0)
+if (err ~= nil) then
+	print('HSet err=',  err);
+	return err
+end
+
+--backup
+--备份
+local ver, err = mm.MMBackup(sid, mid, '')
+if (err ~= nil) then
+	print('MMBackup err=%v',  err);
+	return err
+end
+
+local mmsid2, err = mm.MMOpen(sid, mid, 'last');
+if (err ~= nil) then
+	print('MMOpen err=',  err);
+	return err
+end
+
+local fvs, err = mm.HMGet(mmsid2, key, field0)
+if (err ~= nil) then
+	print('HMGet err=',  err);
+	return err
+end
